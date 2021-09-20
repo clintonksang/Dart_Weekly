8c2bfa6deaac4ab91711d927c193e980393c380a
clintonksang <sangclint0@gmail.com>
2021-09-20 11:43:05 +0300

    Initial

---
+++ Week 1 Dart Objects/objects.dart
@@ -0,0 +1,6 @@
+// Project Url https://github.com/users/clintonksang/projects/3
+
+// Day 1 Go through Documentation
+//  Exercises Coming
+
+// dart.academy/creating-objects-and-classes-in-dart-and-flutter/
